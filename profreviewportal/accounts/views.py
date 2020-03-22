d804f35435c1e6e433a68969a591fb8dd011dcb5
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-03-22 14:35:26 +0530

    implemented accounts (login)

--- profreviewportal/accounts/views.py
+++ profreviewportal/accounts/views.py
@@ -7,12 +7,38 @@ from django.http import HttpResponse
 
 
 def signup_view(request):
-    return HttpResponse('Signup')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # log the user in
+            login(request, user)
+            return HttpResponse('Signed up')
+            # return redirect('homepage.html')  # see this redirect later
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 
 def login_view(request):
-    return HttpResponse('Login')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return HttpResponse('Logged in')
+                # return redirect('homepage.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def logout_view(request):
-    return HttpResponse('Logout')
+    if request.method == 'POST':
+        logout(request)
+        return HttpResponse('Logged out')
+        # return redirect('homepage.html')
